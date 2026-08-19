f5d33dec68dd6d83e1ff98135023f51fe356b572
pedronavarrosantos <p.navarro.nichollas@gmail.com>
2026-08-19 11:46:20 -0300

    imports study

---
+++ python/10_py.imports/impmain.py
@@ -0,0 +1,5 @@
+import impmat
+
+resultado = impmat.somar(10, 5)
+
+print(resultado)
---
+++ python/10_py.imports/imports.study.1.ex1.py
@@ -0,0 +1,17 @@
+from impmat import multiplicar
+
+mult = multiplicar(12, 4)
+
+print(mult)
+
+from impmat import dividir
+
+divid = dividir(100, 50)
+
+print(divid)
+
+from impmat import elevar
+
+poten = elevar(2, 8)
+
+print(poten)
